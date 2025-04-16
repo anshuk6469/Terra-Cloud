2c2b643052c0dc304b5bac6de4d8c957aae0c2cc
Deepanshu Kumar <anshuk6469@gmail.com>
2025-04-16 10:18:31 +0530

    new third rg addded

--- rg2.tf
+++ rg2.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "prodeastus2" {
   name = "deep-prod2"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "prodeastus3" {
+  name = "deep-prod3"
+  location = "eastus"
+}
